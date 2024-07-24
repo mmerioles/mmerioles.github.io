e24e5d00a0ef5252c78f22b4e3f6b7aaa69cc427
mmerioles <matthewmerioles@yahoo.com>
2024-07-24 14:02:24 -0700

    icon changes, refactored titles, spark, skills

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function translateDocument() {
             tr_hello: "Hello, I'm",
             tr_engineer: "Electrical Engineer",
             tr_resume: "Resume",
-            tr_get: "Get To Know More",
+            tr_get: "Find out more",
             tr_aboutme: "About Me",
-            tr_new: "3+ Years of Professional Internship Experience",
+            tr_new: "3+ Professional Internship Experiences",
             tr_ee: "Electrical Engineer",
             tr_edu: "Education",
             tr_deg: "B.Sc. Electrical Engineering",
@@ -29,17 +29,17 @@ function translateDocument() {
             tr_exp: "Experience",
             tr_ucsd: "UC San Diego",
             tr_long: "I'm a passionate individual interested in Data Networks and cool projects. I'm currently working at Northrop Grumman as an Associate Electronics Engineer, where I have built valuable experience designing scalable and robust network architecture and applications. In my free time, I enjoy composing video game and japanese alt rock music.",
-            tr_explore: "Explore My",
-            tr_programming:"Programming Languages",
+            tr_explore: "An Overview of My",
+            tr_programming:"Core Competencies and Tools",
             tr_other: "Other Tools",
             tr_proficient:"Proficient",
             tr_intermediate:"Intermediate",
             tr_basic:"Basic",
-            tr_browse: "Browse My Recent",
+            tr_browse: "My Recent",
             tr_project_1: "Tatacon Project",
             tr_project_2: "RDBMS from Scratch",
             tr_project_3: "Jetson Nano Autonomous Vehicle",
-            tr_touch: "Get in Touch",
+            tr_touch: "Connect With Me",
             tr_github: "Github",
             tr_linkedin: "LinkedIn",
             tr_copyright: "Copyright © 2024 Matthew Merioles. All Rights Reserved.",
@@ -58,7 +58,7 @@ function translateDocument() {
             tr_resume: "履歴書",
             tr_get: "私について",
             tr_aboutme: "プロフィール",
-            tr_new: "3年以上のプロフェッショナルなインターンシップ経験",
+            tr_new: "3回以上のプロフェッショナルなインターンシップ経験",
             tr_ee: "電気工学",
             tr_edu: "学歴",
             tr_deg: "電気工学学士",
@@ -67,7 +67,7 @@ function translateDocument() {
             tr_ucsd: "カリフォルニア大学サンディエゴ校",
             tr_long: "私はデータネットワークと面白いプロジェクトに興味がある情熱家です。現在、ノースロップ・グラマンで電気工エンジニアとして働いており、スケーラブルで堅牢なネットワーク・アーキテクチャとアプリケーションの設計で貴重な経験を積んでいます。余暇には、ゲームミュージックやボカロの作曲を楽しんでいます。",
             tr_explore: "私の",
-            tr_programming:"プログラミング言語",
+            tr_programming:"コア・コンピタンスとツール",
             tr_other: "他のソフトウェア",
             tr_proficient:"上級レベル",
             tr_intermediate:"中級レベル",
@@ -106,3 +106,106 @@ function translateDocument() {
         document.documentElement.setAttribute('lang', newLang);
     }, 500);
 };
+
+// Taken from https://codepen.io/lucasdellabella
+let styleSheet = null;
+
+const SPARK_ELEMENT_WIDTH = 30;
+const DISTANCE = 40;
+
+const RANDOMNESS_ON = true;
+
+/**
+ * Util for creating sequences of css transform steps cleanly
+ */
+function createTransformSteps() {
+  if (Array.from(arguments).length === 0) {
+    throw new Error("arguments to createTransformSteps should never be empty!");
+  }
+
+  const inputSteps = Array.from(arguments);
+  const outputSteps = [inputSteps.shift()];
+  inputSteps.forEach((step, i) => {
+    outputSteps.push(`${outputSteps[i]} ${step}`);
+  });
+
+  return outputSteps;
+}
+
+/**
+ * Creates a new keyframe rule available in css context with a specific spark rotation
+ */
+const dynamicAnimation = (name, rotation) => {
+  if (!styleSheet) {
+    styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    document.head.appendChild(styleSheet);
+  }
+
+  /**
+  Explaining the transform rules
+  1. prepares spark
+  2. shoots the spark out
+  3. keeps the spark in place while scaling it down
+  */
+
+  const randomDist = RANDOMNESS_ON
+    ? Math.floor((Math.random() - 0.5) * DISTANCE * 0.7)
+    : 0;
+
+  const [s1, s2, s3] = createTransformSteps(
+    `translate(-50%, -50%) rotate(${rotation}deg) translate(10px, 0px)`,
+    `translate(${DISTANCE + randomDist}px, 0px) scale(0.5, 0.5)`,
+    `translate(${SPARK_ELEMENT_WIDTH / 2}px, 0) scale(0, 0)`
+  );
+
+  styleSheet.sheet.insertRule(
+    `@keyframes ${name} {
+     0% {
+       transform: ${s1};
+     }
+     70% {
+       transform: ${s2};
+     }
+     100% {
+       transform: ${s3};
+     }
+  }`,
+    styleSheet.length
+  );
+};
+
+document.querySelectorAll('.icon').forEach(icon => {
+  icon.addEventListener('click', (e) => {
+    const center = { x: e.pageX, y: e.pageY };
+    makeBurst(center);
+  });
+});
+
+const makeBurst = (center) => {
+  for (let i = 0; i < 8; i++) {
+    const randomSpace = RANDOMNESS_ON
+      ? Math.floor(-17 + Math.random() * 34)
+      : 0;
+    makeSpark(center, 45 * i + randomSpace);
+  }
+};
+
+/**
+ * Creates a spark
+ */
+const makeSpark = (center, rotation) => {
+  const div = document.createElement("div");
+  const aniName = `disappear_${rotation}`;
+  dynamicAnimation(aniName, rotation);
+
+  div.classList.add("spark");
+  div.style.left = `${center.x}px`;
+  div.style.top = `${center.y}px`;
+  div.style.animation = `${aniName} 500ms ease-out both`;
+  document.body.append(div);
+  setTimeout(() => {
+    document.body.removeChild(div);
+  }, 1000);
+};
+
